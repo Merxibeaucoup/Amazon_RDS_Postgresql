944c81aeadce1c623749dda65b18d95e562c9dce
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-05-25 22:57:02 -0400

    first commit

---
+++ src/main/java/com/edgar/rds/postgres/SpringbootRdsPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.edgar.rds.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRdsPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRdsPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edgar/rds/postgres/SpringbootRdsPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgar.rds.postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRdsPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
